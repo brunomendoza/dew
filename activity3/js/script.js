b3d11a08bd985322962945e8467f5b5d4badeb47
Bruno Mendoza <hola@brunomendoza.es>
2021-03-21 21:39:26 +0000

    Activity 3 done.

--- activity3/js/script.js
+++ activity3/js/script.js
@@ -3,17 +3,23 @@
     var formElement = document.createElement('form');
     var tableWrapper = document.createElement('div');
 
+    var labelColsElement = document.createElement('label');
+    labelColsElement.innerHTML = "Cols&nbsp;";
     var inputColsElement = document.createElement('input');
     inputColsElement.setAttribute('type', 'number');
+    labelColsElement.appendChild(inputColsElement);
 
+    var labelRowsElement = document.createElement('label');
+    labelRowsElement.innerHTML = "Rows&nbsp;";
     var inputRowsElement = document.createElement('input');
     inputRowsElement.setAttribute('type', 'number');
+    labelRowsElement.appendChild(inputRowsElement);
 
     var buttonElement = document.createElement('button');
     buttonElement.innerHTML = "Do it!"
 
-    formElement.appendChild(inputRowsElement);
-    formElement.appendChild(inputColsElement);
+    formElement.appendChild(labelRowsElement);
+    formElement.appendChild(labelColsElement);
     formElement.appendChild(buttonElement);
 
     wrapperElement.appendChild(formElement);
@@ -21,20 +27,26 @@
 
     buttonElement.addEventListener('click', e => {
         e.preventDefault();
-        tableWrapper.appendChild(handleTable(3, 4));
+        var cols = inputColsElement.value;
+        var rows = inputRowsElement.value;
+
+        if (rows > 0 && cols > 0) {
+            tableWrapper.appendChild(handleTable(cols, rows));
+        }
     });
 })();
 
 function handleTable(cols, rows) {
-    var tableElement = document.getElementById('#table');
-
+    var tableElement = document.getElementById('generated');
+    
     if (tableElement) {
         tableElement.remove();
     }
-
+    
     tableElement = document.createElement('table');
-    tableElement.id = 'table';
-
+    tableElement.id = 'generated';
+    tableElement.classList.add(["table"]);
+    
     for (var i = 0; i < rows; i++) {
         var trElement = document.createElement('tr');
 
